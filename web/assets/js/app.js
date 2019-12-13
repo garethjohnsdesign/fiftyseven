e770ec92093e4dfe4f5511487a28d739e69275f2
Gareth Johns <info@garethjohnsdesign.com>
2019-12-13 16:48:12 +0000

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -23,6 +23,18 @@ Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min
 $(document).foundation();
 
 
+// 1. Loader
+// ---------------
+
+if ($("body").hasClass("home")) {
+  $( ".loader" ).removeClass( "hide" );
+  $( ".loader" ).addClass( "loading" );
+  setTimeout( function() {
+    $( ".loader" ).addClass( "loaded" );
+  }, 2500 
+  );
+}
+
 // 4. Animate on Scroll
 // --------------------
 
