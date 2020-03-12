1c0204e7d62adb8d505b223d927b59ab4e4d08b4
Gareth Johns <info@garethjohnsdesign.com>
2020-03-12 09:17:30 +0000

    Cache bust

--- config/general.php
+++ config/general.php
@@ -44,7 +44,7 @@ return [
         'enableTemplateCaching' => false,
 
         // Set the environmental variables
-        'staticAssetsVersion' => 20,
+        'staticAssetsVersion' => 21,
         
     ],
 
